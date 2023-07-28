07eef95a8a7134f13f328f9bacd0e4d1e3973418
MistEO <mistereo@hotmail.com>
2023-07-29 04:13:20 +0800

    Revert "cache: switch to leveldb"
    This reverts commit 2f92146092a02346376db29284ae1ed9a190278d.

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,9 +2,14 @@
 package cache
 
 import (
+	"fmt"
+	"sync"
+
 	log "github.com/sirupsen/logrus"
-	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/opt"
+
+	"github.com/Mrs4s/go-cqhttp/global"
+	"github.com/Mrs4s/go-cqhttp/internal/base"
+	"github.com/Mrs4s/go-cqhttp/internal/btree"
 )
 
 // Media Cache DBs
@@ -16,36 +21,70 @@ var (
 
 // Cache wraps the btree.DB for concurrent safe
 type Cache struct {
-	ldb *leveldb.DB
+	lock sync.RWMutex
+	db   *btree.DB
 }
 
 // Insert 添加媒体缓存
 func (c *Cache) Insert(md5, data []byte) {
-	_ = c.ldb.Put(md5, data, nil)
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	var hash [16]byte
+	copy(hash[:], md5)
+	c.db.Insert(&hash[0], data)
 }
 
 // Get 获取缓存信息
 func (c *Cache) Get(md5 []byte) []byte {
-	got, _ := c.ldb.Get(md5, nil)
-	return got
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+
+	var hash [16]byte
+	copy(hash[:], md5)
+	return c.db.Get(&hash[0])
 }
 
 // Delete 删除指定缓存
 func (c *Cache) Delete(md5 []byte) {
-	_ = c.ldb.Delete(md5, nil)
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	var hash [16]byte
+	copy(hash[:], md5)
+	_ = c.db.Delete(&hash[0])
 }
 
 // Init 初始化 Cache
 func Init() {
-	open := func(typ, path string, cache *Cache) {
-		ldb, err := leveldb.OpenFile(path, &opt.Options{
-			WriteBuffer: 4 * opt.KiB,
-		})
+	node, ok := base.Database["cache"]
+	var conf map[string]string
+	if ok {
+		err := node.Decode(&conf)
 		if err != nil {
-			log.Fatalf("open cache %s db failed: %v", typ, err)
+			log.Fatalf("failed to read cache config: %v", err)
 		}
-		cache.ldb = ldb
 	}
-	open("image", "data/images", &Image)
-	open("video", "data/videos", &Video)
+
+	open := func(typ string, cache *Cache) {
+		file := conf[typ]
+		if file == "" {
+			file = fmt.Sprintf("data/%s.db", typ)
+		}
+		if global.PathExists(file) {
+			db, err := btree.Open(file)
+			if err != nil {
+				log.Fatalf("open %s cache failed: %v", typ, err)
+			}
+			cache.db = db
+		} else {
+			db, err := btree.Create(file)
+			if err != nil {
+				log.Fatalf("create %s cache failed: %v", typ, err)
+			}
+			cache.db = db
+		}
+	}
+	open("image", &Image)
+	open("video", &Video)
 }
